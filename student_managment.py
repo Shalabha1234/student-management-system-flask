9fd2d90ba3defe85af8b897e03de368b8488dc79
Shalabha <shalu18.2004@gmail.com>
2026-08-20 23:17:55 +0530

    Add Procfile for deployment

--- student_managment.py
+++ student_managment.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, redirect
 import json
 
@@ -165,5 +164,6 @@ def update_student(roll):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
